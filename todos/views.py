5f4102ae52ee6edd83e1f7184aa02c73ec48c276
Long Tran <tqlong@gmail.com>
2024-05-02 14:06:01 +0700

    render context to template

--- todos/views.py
+++ todos/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = dict(
+        name='Long'
+    )
+    return render(request, 'index.html', context=context)
