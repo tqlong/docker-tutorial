d9d97fc7e91a1ca7cab60da75d59cf9d55068260
Long Tran <tqlong@gmail.com>
2024-05-02 14:24:14 +0700

    add todo detail view & template

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('details/<int:id>', views.details, name='details'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -7,3 +7,10 @@ def index(request):
         todos=Todo.objects.all()
     )
     return render(request, 'index.html', context=context)
+
+
+def details(request, id):
+    context = dict(
+        todo=Todo.objects.get(id=id)
+    )
+    return render(request, 'details.html', context=context)
