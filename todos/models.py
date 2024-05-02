4b0fe2c5a582f0bf4c41649bc39b771269183a58
Long Tran <tqlong@gmail.com>
2024-05-02 13:51:18 +0700

    Todo model and migrations

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
