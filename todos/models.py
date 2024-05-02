7a6d9b756752b5760929d9913b9e9d5839530f67
Long Tran <tqlong@gmail.com>
2024-05-02 14:01:06 +0700

    create superuser and show Todo table in admin site

--- todos/models.py
+++ todos/models.py
@@ -7,3 +7,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
