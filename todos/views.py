6c76e7b38a77ed8874eb7251104b05b9beed9d93
Long Tran <tqlong@gmail.com>
2024-05-02 14:03:31 +0700

    create index.html template

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    return render(request, 'index.html')
