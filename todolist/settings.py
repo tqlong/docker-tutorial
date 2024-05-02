71cda66e1510c48df0e0f691e42165f9137b3585
Long Tran <tqlong@gmail.com>
2024-05-02 13:41:22 +0700

    first migrate to MariaDB: python manage.py migrate

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'db',
+        'PORT': ''
     }
 }
 
